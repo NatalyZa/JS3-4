60215d9672afeb69ce697a062dc9df6d285988f8
Nataliya <za0305@bk.ru>
2016-05-03 11:01:16 +0300

    Changed js

--- js/script.js
+++ js/script.js
@@ -1,47 +1,59 @@
-var div
-var text = {
-	wrapper: function() {
-		div = document.createElement('div');
-		div.classList.add('wrapper');
-		document.body.appendChild(div);
-		div.innerHTML = '<h1>Тест по программированию</h1>';
+var contentQuestion = [
+	{question: 'Вопрос №1',
+	answers: ['вариант ответа №1','вариант ответа №2','вариант ответа №3']},
+	{question: 'Вопрос №2',
+	answers: ['вариант ответа №1','вариант ответа №2','вариант ответа №3']},
+	{question: 'Вопрос №3',
+	answers: ['вариант ответа №1','вариант ответа №2','вариант ответа №3']}
+];
+
+var wrapper;
+var buttonSub;
+
+var content = {
+	generateDiv: function () {
+		wrapper = document.createElement('div');
+		wrapper.classList.add('wrapper');
 	},
-	quetion_1: function () {
-		var newDiv = document.createElement('div');
-		newDiv.classList.add('test');
-		div.appendChild(newDiv);
-		newDiv.innerHTML = '<h2>1.  Вопрос №1</h2>' 
-		+ '<p><input type="checkbox" name="" value=""> Вариант ответа №1</p>'
-		+ '<p><input type="checkbox" name="" value=""> Вариант ответа №2</p>'
-		+ '<p><input type="checkbox" name="" value=""> Вариант ответа №3</p>'
+	generateHeader: function () {
+		document.body.appendChild(wrapper);
+		wrapper.innerHTML='<h1>Тест по программирорванию</h1>';
 	},
-	quetion_2: function () {
-		var newDiv = document.createElement('div');
-		newDiv.classList.add('test');
-		div.appendChild(newDiv);
-		newDiv.innerHTML = '<h2>2.  Вопрос №2</h2>' 
-		+ '<p><input type="checkbox" name="" value=""> Вариант ответа №1</p>'
-		+ '<p><input type="checkbox" name="" value=""> Вариант ответа №2</p>'
-		+ '<p><input type="checkbox" name="" value=""> Вариант ответа №3</p>'
+	
+	generateQuestion: function () {
+		for (var i = 0; i < contentQuestion.length; i++) {
+			var questions = document.createElement("h2");
+			questions.innerHTML = contentQuestion[i].question;
+			wrapper.appendChild(questions);
+
+			for (var j = 0; j < 3; j++) {
+
+				var answer= document.createElement('p');
+
+				var checkbox = document.createElement('input');
+				checkbox.setAttribute('type', 'checkbox');
+
+				var answerName = document.createElement("span");
+				answerName.innerHTML = ' ' +  contentQuestion[i].answers[j];
+
+				wrapper.appendChild(answer);
+
+				answer.appendChild(checkbox);
+				
+				answer.appendChild(answerName);
+			}
+		}
 	},
-	quetion_3: function () {
-		var newDiv = document.createElement('div');
-		newDiv.classList.add('test');
-		div.appendChild(newDiv);
-		newDiv.innerHTML = '<h2>3.  Вопрос №3</h2>' 
-		+ '<p><input type="checkbox" name="" value=""> Вариант ответа №1</p>'
-		+ '<p><input type="checkbox" name="" value=""> Вариант ответа №2</p>'
-		+ '<p><input type="checkbox" name="" value=""> Вариант ответа №3</p>'
-	},
-	button: function () {
-		var newDiv = document.createElement('p');
-		newDiv.classList.add('button');
-		div.appendChild(newDiv);
-		newDiv.innerHTML = '<input type="submit" name="" value="Проверить мои результаты">'		
-	},
-}
-text.wrapper();
-text.quetion_1();
-text.quetion_2();
-text.quetion_3();
-text.button();
+	generateSubmit: function () {
+    	buttonSub = document.createElement('input');
+    	buttonSub.classList.add('button');
+    	buttonSub.setAttribute('type', 'submit');
+    	buttonSub.setAttribute('value', 'Проверить мои результаты');
+    	wrapper.appendChild(buttonSub);
+    },
+};
+
+content.generateDiv();
+content.generateHeader();
+content.generateQuestion();
+content.generateSubmit();
